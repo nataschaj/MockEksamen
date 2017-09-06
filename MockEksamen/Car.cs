6710d5fbcc1a9ccac643ddbc3d50e50531331a60
natsachaj <natascha_jakobsen@hotmail.com>
2017-09-06 09:26:46 +0200

    OPG 1

---
+++ MockEksamen/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockEksamen
+{
+    public class Car
+    {
+        public int doors { get; set; }
+        public string model { get; set; }
+        public string registrationNo { get; set; }
+        public enum color { Black, White, Gray, Red, Green, Blue };
+
+
+
+        public Car()
+        {
+
+        }
+    }
+}
---
+++ MockEksamen/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockEksamen
+{
+    class Owner
+    {
+        public string address { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+
+        public Owner()
+        {
+
+        }
+    }
+}
